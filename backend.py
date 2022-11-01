4016bcfed0af0c93c67df88c9e7cb0d1539d6e97
varunu311 <varunu311@gmail.com>
2022-11-01 17:51:18 -0400

    Cockroach DB

--- backend.py
+++ backend.py
@@ -1,19 +1,31 @@
-import mysql.connector
+import psycopg2
+import os
 
-mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "Your Password Goes Here ",
-    database = "profydatabase"
-)
-
-mycursor = mydb.cursor()
+conn = psycopg2.connect('postgresql://profy:TRscICM9vdVHM9D-U9M1xw@free-tier14.aws-us-east-1.cockroachlabs.cloud:26257/profydatabase?sslmode=verify-full&options=--cluster%3Dbeige-cuscus-6334')
+mycursor = conn.cursor()
 
 #def getUserName():
 
+def getName(email):
+    sql = "select name from u_data where email=%s"
+    mycursor.execute(sql, (email,))
+    name = mycursor.fetchone()
+    name = ''.join(name)
+
+    return name
+
+
 #def getPassword():
 
+
+
 #def getAllData(Username, Password):
+
+#def addProject():
+
+
+
+#def removeProject():
 
 def validateUser(email, password):
     sql = "select pass from u_data where email=%s"
@@ -26,8 +38,21 @@ def validateUser(email, password):
     else:
         return False
 
-def create_user(email, password):
-    sql = "INSERT INTO u_data (email, pass) VALUES (%s, %s)"
-    val = (email, password)
+def create_user(username,name,email,password):
+    sql = "INSERT INTO u_data (name, username, pass, email) VALUES (%s, %s, %s, %s)"
+    val = (name,username,password, email)
     mycursor.execute(sql, val)
-    mydb.commit()
+    conn.commit()
+
+def create_database():
+    mycursor.execute('CREATE DATABASE profydatabase;')
+    conn.commit()
+    mycursor.execute('USE profydatabase;')
+    mycursor.execute('CREATE TABLE p_data (username TEXT,project TEXT);')
+    conn.commit()
+    mycursor.execute('CREATE TABLE u_data (name TEXT,username TEXT,pass TEXT,email TEXT);')
+    conn.commit()
+
+#Run This On First Run
+#create_database()
+#create_user("varunu311","Varun","varunu311@gmail.com","Upadhyay_12")
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import os                 # os is used to get environment variables IP & PORT
 import backend
+import secrets
+from flask import session
 from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template, redirect, url_for, request
 
 
 app = Flask(__name__)     # create an app
+app.secret_key = "htx0293klp005007dslzew"
 
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
 # get called. What it returns is what is shown as the web page
+
 
 @app.route('/')
 @app.route('/home')
@@ -21,21 +25,35 @@ def Sign_In():
         email = request.form["email"]
         password = request.form["pass"]
         if backend.validateUser(email, password) == True:
-            return redirect(url_for("user",usr=email))
+            session["email"] = email
+            return redirect(url_for("user"))
         else:
             print("Login Failed: Password and Email do not match")
             return render_template('Sign_in.html')
     else:
         return render_template('Sign_in.html')
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route("/email")
+def user():
+    if "email" in session:
+        email = session["email"]
+        return f"<h1>Welcome {backend.getName(email)} To Your Home Page</h1>"
+    else:
+        return redirect(url_for("Sign_In"))
 
-@app.route('/signup')
-@app.route('/register')
+
+@app.route('/signup', methods=["POST","GET"])
+@app.route('/register', methods=["POST","GET"])
 def Sign_Up():
-    return render_template('register.html')
+    if request.method == "POST":
+        name = request.form["name"]
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["pass"]
+        backend.create_user(username, name, email, password)
+        return redirect(url_for("Sign_In"))
+    else:
+        return render_template('Register.html')
 
 @app.route('/logged_in')
 def landing_page():
