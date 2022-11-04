ee50be476bbcc2ff2eaeeed6e56867b0ae5c50c4
varunu311 <varunu311@gmail.com>
2022-11-04 17:25:34 -0400

    Updated  User.html

--- backend.py
+++ backend.py
@@ -2,44 +2,67 @@ import psycopg2
 import os
 
 conn = psycopg2.connect('postgresql://profy:TRscICM9vdVHM9D-U9M1xw@free-tier14.aws-us-east-1.cockroachlabs.cloud:26257/profydatabase?sslmode=verify-full&options=--cluster%3Dbeige-cuscus-6334')
-mycursor = conn.cursor()
+mc = conn.cursor()
 
 #def getUserName():
 
 def getName(email):
     sql = "select name from u_data where email=%s"
-    mycursor.execute(sql, (email,))
-    name = mycursor.fetchone()
+    mc.execute(sql, (email,))
+    name = mc.fetchone()
     name = ''.join(name)
-
+    print(name)
     return name
 
 
-#def getPassword():
+def getPassword(email):
+    sql = "select password from u_data where email=%s"
+    mc.execute(sql, (email,))
+    password = mc.fetchone()
+    password = ''.join(password)
+    return password
 
+def getAllData(email):
+    sql = "select * from u_data where email=%s"
+    mc.execute(sql, (email,))
+    data = mc.fetchall()
+    print(data)
+    return data
 
-
-#def getAllData(Username, Password):
+def getAllProjects(username):
+    sql = "select project from p_data where username=%s"
+    mc.execute(sql, (username,))
+    projects = mc.fetchall()
+    print(projects)
+    return projects
 
 def addProject(username, project):
     sql = "INSERT INTO p_data (username, project) VALUES (%s, %s)"
     val = (username,project)
-    mycursor.execute(sql,val)
+    mc.execute(sql,val)
     conn.commit
-    
+    print("Done")
+
+#def removeProject():
+def removeProject(username,project):
+    sql = "DELETE FROM p_data WHERE username=%s AND project=%s"
+    val = (username,project)
+    mc.execute(sql,val)
+    conn.commit
+    print("Done")
+
 def getUsername(email):
     sql = "select username from u_data where email=%s"
-    mycursor.execute(sql, (email,))
-    username = mycursor.fetchone()
+    mc.execute(sql, (email,))
+    username = mc.fetchone()
     username = ''.join(username)
     return username
 
-#def removeProject():
 
 def validateUser(email, password):
     sql = "select pass from u_data where email=%s"
-    mycursor.execute(sql, (email,))
-    c_pass = mycursor.fetchone()
+    mc.execute(sql, (email,))
+    c_pass = mc.fetchone()
     c_pass = ''.join(c_pass)
 
     if c_pass == password:
@@ -50,17 +73,19 @@ def validateUser(email, password):
 def create_user(username,name,email,password):
     sql = "INSERT INTO u_data (name, username, pass, email) VALUES (%s, %s, %s, %s)"
     val = (name,username,password, email)
-    mycursor.execute(sql, val)
+    mc.execute(sql, val)
     conn.commit()
+    print("Done")
 
 def create_database():
-    mycursor.execute('CREATE DATABASE profydatabase;')
+    mc.execute('CREATE DATABASE profydatabase;')
     conn.commit()
-    mycursor.execute('USE profydatabase;')
-    mycursor.execute('CREATE TABLE p_data (username TEXT,project TEXT);')
+    mc.execute('USE profydatabase;')
+    mc.execute('CREATE TABLE p_data (username TEXT,project TEXT);')
     conn.commit()
-    mycursor.execute('CREATE TABLE u_data (name TEXT,username TEXT,pass TEXT,email TEXT);')
+    mc.execute('CREATE TABLE u_data (name TEXT,username TEXT,pass TEXT,email TEXT);')
     conn.commit()
+    print("Done")
 
 #Run This On First Run
 #create_database()
